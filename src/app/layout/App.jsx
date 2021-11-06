d300b950937204efcb20dc2915c59b3ea4186e90
larsthrasher <thrasherlars@gmail.com>
2021-11-06 12:21:36 -0400

    basic layout, form, state

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,16 @@
-import './styles.css';
+import React, { useState } from "react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/NavBar";
+import { Container } from "semantic-ui-react";
 
-function App() {
+export default function App() {
+  const [formOpen, setFormOpen] = useState(false);
   return (
-    <div className="App">
-    <h1>Funschit</h1>
-    </div>
+    <>
+      <NavBar setFormOpen={setFormOpen} />
+      <Container className="main">
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} />
+      </Container>
+    </>
   );
 }
-
-export default App;
